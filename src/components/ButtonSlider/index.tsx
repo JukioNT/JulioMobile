84b874e7dd04a88d81684b9d51b4e1a52b2df0fe
JukioNT <jjulio2468@gmail.com>
2023-03-06 10:36:26 -0300

    ButtonSlider and TitleSlider

---
+++ src/components/ButtonSlider/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { TouchableOpacity } from "react-native/types";
+import { styles } from "./styles";
+
+export interface IBSlider {
+    onPressI: () => void
+}
+
+export function ButtonSlider({onPressI}: IBSlider){
+    return(
+        <TouchableOpacity style={styles.ball} onPress={onPressI} />
+    )
+}
---
+++ src/components/ButtonSlider/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native/types";
+import { colors } from "../../styles/colors";
+
+export const styles = StyleSheet.create({
+    ball: {
+        width: 30,
+        height: 30,
+        backgroundColor: colors.primary,
+        borderRadius: 15,
+    }
+})
