400235b508ffb8699e23fde98ae44fee77807157
JukioNT <jjulio2468@gmail.com>
2023-03-07 13:42:03 -0300

    Pag Principal

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { ButtonSlider as ComponentButtonSlider } from "./ButtonSlider"
+export { ListMarker as ComponentListMaker } from "./ListMarker"
+export { Marker as ComponentMarker } from "./Marker"
+export { TitleSlider as ComponentTitleSlider } from "./TitleSlider"
---
+++ src/screens/Slider1/index.tsx
@@ -0,0 +1,8 @@
+import { FlatList, View } from "react-native/types";
+import { IPage } from "../../../App";
+import { ComponentButtonSlider, ComponentMarker, ComponentTitleSlider } from "../../components"
+import { styles } from "./styles";
+
+export function Slider1({ setPageI }: IPage){
+    const slide1 = require("../../assets/slide1.png")
+}
